069f06599ab7dbc80bc3c664e374dc94abbd3326
Ylan Segal <ylan@segal-family.com>
2016-10-07 14:57:03 -0700

    Switch to using Spec2, more Rspec-like

--- spec/franklin_spec.cr
+++ spec/franklin_spec.cr
@@ -2,6 +2,6 @@ require "./spec_helper"
 
 describe Franklin do
   it "has a version number" do
-    Franklin::VERSION.should_not be_nil
+    expect(Franklin::VERSION).not_to be_nil
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,5 @@
-require "spec"
+require "spec2"
 require "../src/franklin"
+
+include Spec2::GlobalDSL
+Spec2.doc
