afa7f06fbac8840d5c986f942d2858124277f9d6
Ylan Segal <ylan@segal-family.com>
2016-10-13 18:15:50 -0700

    Remove unnecessary nested context

--- spec/franklin/config_spec.cr
+++ spec/franklin/config_spec.cr
@@ -3,28 +3,26 @@ require "yaml"
 
 module Franklin
   describe Config do
-    describe "#initialize" do
-      subject { Config.from_yaml(yaml) }
-      let(yaml) {
-        <<-END
-        ---
-        :libraries:
-          - :name: #{library.name}
-            :url: #{library.url}
-        :default_type: #{default_type}
-        END
-       }
-      let(library) { Library.new("Alexandria Library", "http://alexandria.book") }
-      let(default_type) { "eBook" }
+    subject { Config.from_yaml(yaml) }
+    let(yaml) {
+      <<-END
+      ---
+      :libraries:
+        - :name: #{library.name}
+          :url: #{library.url}
+      :default_type: #{default_type}
+      END
+     }
+    let(library) { Library.new("Alexandria Library", "http://alexandria.book") }
+    let(default_type) { "eBook" }
 
-      context "YAML deserilization" do
-        it "maps default_type" do
-          expect(subject.default_type).to eq(default_type)
-        end
+    context "YAML deserilization" do
+      it "maps default_type" do
+        expect(subject.default_type).to eq(default_type)
+      end
 
-        it "maps libraries" do
-          expect(subject.libraries).to eq([library])
-        end
+      it "maps libraries" do
+        expect(subject.libraries).to eq([library])
       end
     end
   end
