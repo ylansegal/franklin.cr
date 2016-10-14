82d782abd6e574b11998c4569f2485998c1df04d
Ylan Segal <ylan@segal-family.com>
2016-10-13 18:25:53 -0700

    Always require argument, but allow nil and set to default

--- src/franklin/config.cr
+++ src/franklin/config.cr
@@ -16,7 +16,8 @@ module Franklin
       }
     )
 
-    def self.from_file(file : String = DEFAULT_FILE_LOCATION)
+    def self.from_file(file : String | Nil = nil)
+      file ||= DEFAULT_FILE_LOCATION
       from_yaml(File.read(file))
     end
   end
