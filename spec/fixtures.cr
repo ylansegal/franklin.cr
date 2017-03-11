572854c938a82a051a5bcb0e33662472de06d03e
Ylan Segal <ylan@segal-family.com>
2017-03-11 11:17:06 -0800

    Add fixtures for Library

---
+++ spec/fixtures.cr
@@ -0,0 +1,11 @@
+module Franklin
+  struct Library
+    def self.fixture
+      new("San Francisco Public Library", "http://sfpl.lib.overdrive.com")
+    end
+
+    def self.random_fixture
+      new(SecureRandom.hex, "http://#{SecureRandom.hex}.com")
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,5 +1,6 @@
 require "spec2"
 require "../src/franklin"
+require "./fixtures"
 
 include Spec2::GlobalDSL
 Spec2.doc
