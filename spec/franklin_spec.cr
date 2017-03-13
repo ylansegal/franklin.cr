157341c6290fa54530d88c3277163b496b76e3a8
Ylan Segal <ylan@segal-family.com>
2017-03-13 14:55:52 -0700

    Add end-to-end integration test, wihtout network interaction

--- spec/franklin_spec.cr
+++ spec/franklin_spec.cr
@@ -4,4 +4,22 @@ describe Franklin do
   it "has a version number" do
     expect(Franklin::VERSION).not_to be_nil
   end
+
+  describe ".run" do
+    let(:example_config_path) { File.join(__DIR__, "example_franklin_config.yml") }
+    let(:test_connection) { Cossack::TestConnection.new.tap { |con|
+        con.stub_get("/search", {200, File.read("spec/seveneves_search.html")})
+      }
+    }
+
+    it "integrates end-to-end" do
+      expect {
+        Franklin.run("Seveneves",
+                     config_path: example_config_path,
+                     type: nil,
+                     io: IO::Memory.new,
+                     connection: test_connection)
+      }.not_to raise_error
+    end
+  end
 end
--- src/franklin.cr
+++ src/franklin.cr
@@ -1,13 +1,13 @@
 require "./franklin/*"
 
 module Franklin
-  def self.run(search_terms, opts)
-    config = Config.from_file(opts.fetch(:config_path, nil))
+  def self.run(search_terms : String, config_path : String, type : String | Nil, io : IO = STDOUT, connection = nil)
+    config = Config.from_file(config_path)
     per_library_results = config.libraries.map { |library|
-      Search.new(library).perform(search_terms)
+      Search.new(library).perform(search_terms, connection)
     }
     results = Collate.new.perform(per_library_results)
-    filtered_results = TypeFilter.new(opts.fetch(:type, nil)).perform(results)
-    ConsoleReport.new(search_terms, filtered_results).to_s(STDOUT)
+    filtered_results = TypeFilter.new(type || config.default_type).perform(results)
+    ConsoleReport.new(search_terms, filtered_results).to_s(io)
   end
 end
