7458c0331a0a3fc88a777bdd52f1e12da8a60f37
Ylan Segal <ylan@segal-family.com>
2017-03-13 10:04:22 -0700

    Uncomment to ensure compiler can proceed

--- src/franklin.cr
+++ src/franklin.cr
@@ -6,7 +6,7 @@ module Franklin
     per_library_results = config.libraries.map { |library|
       Search.new(library).perform(search_terms)
     }
-    # results = Collate.new.perform(per_library_results)
+    results = Collate.new.perform(per_library_results)
     # filetered_results = TypeFilter.new(opts.fetch(:type, nil)).perform(results)
     # ConsoleReport.new(search_terms, filtered_results).print_to_out(opts.fetch(:out, STDOUT))
   end
