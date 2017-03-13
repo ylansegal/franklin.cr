8b34f33096528d8ac0ac362b13553bafbfa9930d
Ylan Segal <ylan@segal-family.com>
2017-03-13 13:20:10 -0700

    Add AvailabilityDescription

---
+++ spec/franklin/availability_description_spec.cr
@@ -0,0 +1,36 @@
+require "../spec_helper"
+
+module Franklin
+  describe AvailabilityDescription do
+    describe "#to_s" do
+      let(:result) { AvailabilityDescription.new(availability).to_s }
+      let(:library) { Library.fixture }
+
+      context "when available_copies is greater 0" do
+        let(:availability) { Availability.new(library, 10, 2, 0) }
+
+        it "describes it as available" do
+          expect(result).to eq("Available @ #{availability.library.name}")
+        end
+      end
+
+      context "when available_copies is 0" do
+        context "when wait_list_size is reported" do
+          let(:availability) { Availability.new(library, 4, 0, 13) }
+
+          it "describes the people per copy" do
+            expect(result).to eq("3.3 people/copy @ #{availability.library.name}")
+          end
+        end
+
+        context "when wait_list_size is not reported" do
+          let(:availability) { Availability.new(library, 4, 0, 0) }
+
+          it "describes unkown people per copy" do
+            expect(result).to eq("Unknown people/copy @ #{availability.library.name}")
+          end
+        end
+      end
+    end
+  end
+end
---
+++ src/franklin/availability_description.cr
@@ -0,0 +1,28 @@
+module Franklin
+  class AvailabilityDescription
+    def initialize(@availability : Availability)
+    end
+
+    forward_missing_to @availability
+
+    def to_s
+      "#{copies_information} @ #{library.name}"
+    end
+
+    private def copies_information
+      available? ? "Available" : "#{copies_per_person} people/copy"
+    end
+
+    private def available?
+      available_copies > 0
+    end
+
+    private def copies_per_person
+      wait_list? ? (wait_list_size.to_f / total_copies.to_f).round(1) : "Unknown"
+    end
+
+    private def wait_list?
+      wait_list_size > 0
+    end
+  end
+end
