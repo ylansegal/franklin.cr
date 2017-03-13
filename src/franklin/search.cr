99e847c19b51897f5ba1c32f62a8732ba94fe1d8
Ylan Segal <ylan@segal-family.com>
2017-03-13 16:57:44 -0700

    Harden parser in case returned JSON doesn't have all keys

--- src/franklin/search.cr
+++ src/franklin/search.cr
@@ -26,15 +26,16 @@ module Franklin
     private def extract_json(result_page : XML::Node)
       script_tag = result_page.xpath_nodes("//script").map(&.content).select { |n| n =~ JS_VARIABLE }.first
       var_assignment_line = script_tag.lines.find { |line| line =~ JS_VARIABLE }
-      raw_javascript_object = var_assignment_line.scan(/{.*}/).first[0] if var_assignment_line
+      matches = var_assignment_line.scan(/{.*}/).[0]? if var_assignment_line
+      raw_javascript_object = matches[0]? if matches
       JSON.parse(raw_javascript_object) if raw_javascript_object
     end
 
-    def parse(json : Nil)
+    private def parse(json : Nil)
       {} of Item => Availability
     end
 
-    def parse(json : JSON::Any)
+    private def parse(json : JSON::Any)
       result = {} of Item => Availability
       json.each { |id, data|
         result[parse_item(id.as_s, data)] = parse_availability(data)
@@ -42,12 +43,27 @@ module Franklin
       result
     end
 
-    def parse_item(id, data)
+    private def parse_item(id, data)
       Item.new(id, data["title"].as_s, data["firstCreatorName"].as_s, data["type"]["name"].as_s)
     end
 
-    def parse_availability(data)
-      Availability.new(library, data["ownedCopies"].as_i, data["availableCopies"].as_i, data["holdsCount"].as_i)
+    private def parse_availability(data)
+      Availability.new(library,
+                       extract(data, "ownedCopies"),
+                       extract(data, "availableCopies"),
+                       extract(data, "holdsCount"))
+    end
+
+    private def extract(data, key)
+      json_to_i(data[key]?)
+    end
+
+    private def json_to_i(json : JSON::Any)
+      json.as_i
+    end
+
+    private def json_to_i(json : Nil)
+      0
     end
 
     private def search_path(search_terms : String)
