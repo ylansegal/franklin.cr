b48b11b25a6c4062560ce310df3077c8f28c3dfe
Ylan Segal <ylan@segal-family.com>
2017-03-14 15:33:44 -0700

    Hook ConcurrentSearch

--- src/franklin.cr
+++ src/franklin.cr
@@ -3,9 +3,7 @@ require "./franklin/*"
 module Franklin
   def self.run(search_terms : String, config_path : String, filter : String | Nil, io : IO = STDOUT)
     config = Config.from_file(config_path)
-    per_library_results = config.libraries.map { |library|
-      Search.new(library).perform(search_terms)
-    }
+    per_library_results = ConcurrentSearch(Search).new(config.libraries).perform(search_terms)
     results = Collate.new.perform(per_library_results)
     filtered_results = TypeFilter.new(filter || config.default_type).perform(results)
     ConsoleReport.new(search_terms, filtered_results).to_s(io)
