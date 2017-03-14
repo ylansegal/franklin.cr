4f50c8f4de548247c115c72a68fedbe93842092d
Ylan Segal <ylan@segal-family.com>
2017-03-14 09:05:06 -0700

    Parse JSON to struct...
    Takes advantage of the rich facilities in crystal to map JSON to
    known types. In order to be able to  parse, needs an initial known
    key, so we provide one artifically.
    
    { "123": {}, "456": {} }
    
    Becomes
    
    { "data": { "123": {}, "456": {} } }
    
    This allows the first parsing to be of type Hash(String, Overdrive:Item).
    Everything else is done by the mapping macro and type system

--- spec/franklin/search_spec.cr
+++ spec/franklin/search_spec.cr
@@ -2,12 +2,13 @@ require "../spec_helper"
 
 module Franklin
   describe Search do
+    subject { Search.new(library) }
+    let(:library) { Library.fixture }
+    let(:search_terms) { "Prelude to Foundation" }
+
     context "#perform" do
-      subject { Search.new(library) }
-      let(:library) { Library.fixture }
-      let(:results) { subject.perform(search_term) }
+      let(:results) { subject.perform(search_terms) }
       let(:items) { results.keys }
-      let(:search_term) { "Prelude to Foundation" }
       let(:response_body) { File.read("spec/prelude_to_foundation_search.html") }
 
       it "returns expected results" do
@@ -17,8 +18,16 @@ module Franklin
                .to_return(status: 200, body: response_body)
 
         expect(items.map(&.format).sort).to eq(["eBook", "Audiobook"].sort)
-        expect(items.map(&.title).first).to match(/#{search_term}/)
+        expect(items.map(&.title).first).to match(/#{search_terms}/)
         expect(items.map(&.author).first).to eq("Isaac Asimov")
+      end
+    end
+
+    context "search_url" do
+      let(:search_url) { subject.search_url(search_terms) }
+
+      it "appends the search path and query" do
+        expect(search_url).to eq("#{library.url}/search?query=Prelude+to+Foundation")
       end
 
       context "when a libarary contains an ending /" do
@@ -26,11 +35,8 @@ module Franklin
         let(:expected_url) { "https://alexandria.org/search?query=Prelude+to+Foundation" }
         let(:library) { Library.new("Library Of Alexandria", url) }
 
-        it "properly sets the path for the URI" do
-          WebMock.stub(:get, expected_url)
-                 .to_return(status: 200, body: response_body)
-
-          expect { results }.not_to raise_error
+        it "handles setting the path without repeating /" do
+          expect(search_url).to eq(expected_url)
         end
       end
     end
--- src/franklin/search.cr
+++ src/franklin/search.cr
@@ -12,8 +12,15 @@ module Franklin
     def initialize(@library)
     end
 
-    def perform(search_term : String) : Hash(Item, Availability)
-      results_page = search_library(search_term)
+    def search_url(search_terms : String) : String
+      URI.parse(library.url).tap { |uri|
+        uri.path = "/search"
+        uri.query = HTTP::Params.build { |params| params.add("query", search_terms) }
+      }.to_s
+    end
+
+    def perform(search_terms : String) : Hash(Item, Availability)
+      results_page = search_library(search_terms)
       results_json = extract_json(results_page)
       parse(results_json)
     end
@@ -23,54 +30,51 @@ module Franklin
       XML.parse(response.body)
     end
 
-    private def search_url(search_terms)
-      URI.parse(library.url).tap { |uri|
-        uri.path = "/search"
-        uri.query = HTTP::Params.build { |params| params.add("query", search_terms) }
-      }
-    end
-
     private def extract_json(result_page : XML::Node)
       script_tag = result_page.xpath_nodes("//script").map(&.content).select { |n| n =~ JS_VARIABLE }.first
       var_assignment_line = script_tag.lines.find { |line| line =~ JS_VARIABLE }
       matches = var_assignment_line.scan(/{.*}/).[0]? if var_assignment_line
       raw_javascript_object = matches[0]? if matches
-      JSON.parse(raw_javascript_object) if raw_javascript_object
+      "{ \"data\": #{raw_javascript_object}}" if raw_javascript_object
     end
 
     private def parse(json : Nil)
       {} of Item => Availability
     end
 
-    private def parse(json : JSON::Any)
+    private def parse(raw_data : String)
       result = {} of Item => Availability
-      json.each { |id, data|
-        result[parse_item(id.as_s, data)] = parse_availability(data)
-      }
+      Overdrive::Data.from_json(raw_data).data.each do |id, oitem|
+        item = Franklin::Item.new(id, oitem.title, oitem.firstCreatorName, oitem.type.name)
+        availability = Franklin::Availability.new(library, oitem.ownedCopies, oitem.availableCopies, oitem.holdsCount)
+        result[item] = availability
+      end
       result
     end
+  end
 
-    private def parse_item(id, data)
-      Item.new(id, data["title"].as_s, data["firstCreatorName"].as_s, data["type"]["name"].as_s)
+  module Overdrive
+    struct Data
+      JSON.mapping(
+        data: Hash(String, Overdrive::Item)
+      )
     end
 
-    private def parse_availability(data)
-      Availability.new(library,
-                       extract(data, "ownedCopies"),
-                       extract(data, "availableCopies"),
-                       extract(data, "holdsCount"))
+    struct Item
+      JSON.mapping(
+        title: String,
+        firstCreatorName: String,
+        type: Type,
+        ownedCopies: { type: Int32, default: 0 },
+        availableCopies: { type: Int32, default: 0 },
+        holdsCount: { type: Int32, default: 0 }
+      )
     end
 
-    private def extract(data, key)
-      json_to_i(data[key]?)
-    end
-
-    private def json_to_i(json : JSON::Any)
-      json.as_i
-    end
-
-    private def json_to_i(json : Nil)
-      0
+    struct Type
+      JSON.mapping(
+        name: String
+      )
     end
   end
 end
