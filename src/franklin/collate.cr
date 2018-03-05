a78a418de116f44bad2801583294d68405520b2f
Ylan Segal <ylan@segal-family.com>
2018-03-05 15:30:58 -0800

    Remove unused code

--- src/franklin/collate.cr
+++ src/franklin/collate.cr
@@ -3,7 +3,6 @@ module Franklin
     def perform(search_results) : Hash(Item, Array(Availability))
       items = Set.new(search_results.flat_map(&.keys))
       items.each_with_object({} of Item => Array(Availability)) { |item, collation|
-        search_results.map { |result| result[item]? }.compact
         collation[item] = search_results.map { |result| result[item]? }.compact
       }
     end
