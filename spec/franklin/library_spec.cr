e9ca1f501cf76c5eb94996979b83357696961c33
Ylan Segal <ylan@segal-family.com>
2018-11-05 14:50:04 -0800

    Convert library_spec

--- spec/franklin/library_spec.cr
+++ spec/franklin/library_spec.cr
@@ -2,20 +2,20 @@ require "../spec_helper"
 
 module Franklin
   describe Library do
-    subject { Library.new(name, url) }
-    let(name) { "The Library Of Alexandria" }
-    let(url) { "https://alexandria.book" }
+    name = "The Library Of Alexandria"
+    url = "https://alexandria.book"
+    subject = Library.new(name, url)
 
     it "has a name" do
-      expect(subject.name).to eq(name)
+      subject.name.should eq(name)
     end
 
     it "has a url" do
-      expect(subject.url).to eq(url)
+      subject.url.should eq(url)
     end
 
     it "is equal if fields are equal" do
-      expect(subject).to eq(Library.new(name, url))
+      subject.should eq(Library.new(name, url))
     end
   end
 end
