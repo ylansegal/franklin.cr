1a1af8f1d185a7267fc7048ba91810d29c8793e8
Ylan Segal <ylan@segal-family.com>
2018-11-05 15:12:37 -0800

    Convert collate_spec

--- spec/franklin/collate_spec.cr
+++ spec/franklin/collate_spec.cr
@@ -2,31 +2,28 @@ require "../spec_helper"
 
 module Franklin
   describe Collate do
-    let(:results) { Collate.new.perform([search_results_1, search_results_2]) }
-
     describe "#perform (combines multiple search results)" do
-      let(:search_results_1) {
-        {
-          item_1 => availability_1,
-          item_2 => availability_2,
-        }
+      item_1 = Item.random_fixture
+      item_2 = Item.random_fixture
+      item_3 = Item.random_fixture
+      availability_1 = Availability.random_fixture
+      availability_2 = Availability.random_fixture(1)
+      availability_3 = Availability.random_fixture(0)
+      availability_4 = Availability.random_fixture
+      search_results_1 = {
+        item_1 => availability_1,
+        item_2 => availability_2,
       }
-      let(:search_results_2) {
-        {
-          item_2 => availability_3,
-          item_3 => availability_4,
-        }
+      search_results_2 = {
+        item_2 => availability_3,
+        item_3 => availability_4,
       }
-      let(:item_1) { Item.random_fixture }
-      let(:item_2) { Item.random_fixture }
-      let(:item_3) { Item.random_fixture }
-      let(:availability_1) { Availability.random_fixture }
-      let(:availability_2) { Availability.random_fixture(1) }
-      let(:availability_3) { Availability.random_fixture(0) }
-      let(:availability_4) { Availability.random_fixture }
+
 
       it "includes all items and their availabilities" do
-        expect(results).to eq({
+        results = Collate.new.perform([search_results_1, search_results_2])
+
+        results.should eq({
           item_1 => [availability_1],
           item_2 => [availability_2, availability_3],
           item_3 => [availability_4],
@@ -35,17 +32,19 @@ module Franklin
     end
 
     context "ordering of availabilities" do
-      let(:results) { Collate.new.perform([search_results_1, search_results_2, search_results_3]) }
-      let(:search_results_1) { { item_1 => availability_3 } }
-      let(:search_results_2) { { item_1 => availability_1 } }
-      let(:search_results_3) { { item_1 => availability_2 } }
-      let(:item_1) { Item.random_fixture }
-      let(:availability_1) { Availability.new(Library.random_fixture, 10, 3, 0) }
-      let(:availability_2) { Availability.new(Library.random_fixture, 20, 0, 4) }
-      let(:availability_3) { Availability.new(Library.random_fixture, 10, 0, 3) }
+      item_1 = Item.random_fixture
+      availability_1 = Availability.new(Library.random_fixture, 10, 3, 0)
+      availability_2 = Availability.new(Library.random_fixture, 20, 0, 4)
+      availability_3 = Availability.new(Library.random_fixture, 10, 0, 3)
+      search_results_1 = { item_1 => availability_3 }
+      search_results_2 = { item_1 => availability_1 }
+      search_results_3 = { item_1 => availability_2 }
+
 
       it "shows available copies first, then by copies per person" do
-        expect(results).to eq({
+        results = Collate.new.perform([search_results_1, search_results_2, search_results_3])
+
+        results.should eq({
           item_1 => [availability_1, availability_2, availability_3]
         })
       end
