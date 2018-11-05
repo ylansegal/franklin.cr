f2eecdf0a3e607e454b2e95810f29f182ba82595
Ylan Segal <ylan@segal-family.com>
2018-11-05 14:41:34 -0800

    Convert concurrent_search

--- spec/franklin/concurrent_search_spec.cr
+++ spec/franklin/concurrent_search_spec.cr
@@ -15,53 +15,48 @@ module Franklin
   end
 
   describe ConcurrentSearch do
-    subject { ConcurrentSearch.new(libraries) }
-    let(:libraries) { [library_1, library_2] }
-    let(:library_1) { Library.random_fixture }
-    let(:library_2) { Library.random_fixture }
+    library_1 = Library.random_fixture
+    library_2 = Library.random_fixture
+    libraries = [library_1, library_2]
+    subject = ConcurrentSearch.new(libraries)
+
 
     describe "#perform" do
-      let(:search_results_1) {
-        {
+      item_1 = Item.random_fixture
+      item_2 = Item.random_fixture
+      item_3 = Item.random_fixture
+      availability_1 = Availability.random_fixture
+      availability_2 = Availability.random_fixture
+      availability_3 = Availability.random_fixture
+      availability_4 = Availability.random_fixture
+      search_results_1 = {
           item_1 => availability_1,
           item_2 => availability_2,
         }
-      }
-      let(:search_results_2) {
-        {
+      search_results_2 = {
           item_2 => availability_3,
           item_3 => availability_4,
         }
-      }
-      let(:item_1) { Item.random_fixture }
-      let(:item_2) { Item.random_fixture }
-      let(:item_3) { Item.random_fixture }
-      let(:availability_1) { Availability.random_fixture }
-      let(:availability_2) { Availability.random_fixture }
-      let(:availability_3) { Availability.random_fixture }
-      let(:availability_4) { Availability.random_fixture }
-      let(:search_terms) { "Harry Potter" }
-      let(:searchers) {
-        [
+
+      search_terms = "Harry Potter"
+      searchers = [
           Test::Search.new(library_1, search_results_1).as(SearchInterface),
           Test::Search.new(library_2, search_results_2).as(SearchInterface),
         ]
-      }
-      let(:results) { subject.perform(search_terms) }
 
-      before {
-        subject.searchers = searchers
-      }
+      subject.searchers = searchers
+      results = subject.perform(search_terms)
 
       it "returns collated results from multiple libraries" do
-        expect(results).to eq([search_results_1, search_results_2])
+        results.should eq([search_results_1, search_results_2])
       end
 
       context "when no search term is provided" do
-        let(:search_terms) { "" }
 
         it "raises an ArgumentError" do
-          expect { results }.to raise_error(ArgumentError)
+          expect_raises(ArgumentError) do
+            subject.perform("")
+          end
         end
       end
     end
