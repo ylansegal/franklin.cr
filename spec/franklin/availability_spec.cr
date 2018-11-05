6215148af6a4a7861fff6b034e8d39fadba707c1
Ylan Segal <ylan@segal-family.com>
2018-11-05 14:42:52 -0800

    Convert availability_spec

--- spec/franklin/availability_spec.cr
+++ spec/franklin/availability_spec.cr
@@ -2,30 +2,30 @@ require "../spec_helper"
 
 module Franklin
   describe Availability do
-    subject { Availability.new(library, total_copies, available_copies, wait_list_size) }
-    let(:library) { Library.fixture }
-    let(:total_copies) { 11 }
-    let(:available_copies) { 0 }
-    let(:wait_list_size) { 3 }
+    library = Library.fixture
+    total_copies = 11
+    available_copies = 0
+    wait_list_size = 3
+    subject = Availability.new(library, total_copies, available_copies, wait_list_size)
 
     it "has a library" do
-      expect(subject.library).to eq(library)
+      subject.library.should eq(library)
     end
 
     it "has total_copies" do
-      expect(subject.total_copies).to eq(total_copies)
+      subject.total_copies.should eq(total_copies)
     end
 
     it "has available_copies" do
-      expect(subject.available_copies).to eq(available_copies)
+      subject.available_copies.should eq(available_copies)
     end
 
     it "has wait_list_size" do
-      expect(subject.wait_list_size).to eq(wait_list_size)
+      subject.wait_list_size.should eq(wait_list_size)
     end
 
     it "reports persons_per_copy" do
-      expect(subject.persons_per_copy).to eq(0.3)
+      subject.persons_per_copy.should eq(0.3)
     end
   end
 end
